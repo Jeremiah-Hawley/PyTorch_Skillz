c12909e81d96e70294fb9639a651d98f12366705
Jeremiah Hawley <jhawley6@uwyo.edu>
2026-07-10 07:53:26 -0600

    fixed bug in script

--- environment_init.sh
+++ environment_init.sh
@@ -1,8 +1,8 @@
-#!/bin/env bas0
+#!/bin/env bash
 if [[ $# -ne 0 ]]; then
   echo " === creating conda environment === ";
   conda create --name $1 python=3.11 numpy pytorch torchvision numpy matplotlib scikit-learn &&
-  conda activate $1;
+  conda activate $1 &&
   mkdir data;
 else
   echo "please pass an argument containing a name for the environment"
